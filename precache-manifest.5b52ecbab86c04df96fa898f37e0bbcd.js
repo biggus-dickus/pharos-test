ee220a1828b0c2044a37746c4cfc1858e3390d41
maxm <maxm@nastasha.com>
2019-11-18 11:53:12 +0200

    Updates

---
+++ precache-manifest.5b52ecbab86c04df96fa898f37e0bbcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca49953b683c932df8a4973678a7548f",
+    "url": "/pharos-test/index.html"
+  },
+  {
+    "revision": "0796c3e6e4714a399eb7",
+    "url": "/pharos-test/static/css/main.142c1832.chunk.css"
+  },
+  {
+    "revision": "66f44e6f804e0d49cddb",
+    "url": "/pharos-test/static/js/2.a169a41e.chunk.js"
+  },
+  {
+    "revision": "0796c3e6e4714a399eb7",
+    "url": "/pharos-test/static/js/main.f9249acb.chunk.js"
+  },
+  {
+    "revision": "18919f4031b4fb786b81",
+    "url": "/pharos-test/static/js/runtime-main.7e20e193.js"
+  },
+  {
+    "revision": "56303b37fe761a7e1edb1b2c4dc4042a",
+    "url": "/pharos-test/static/media/sprite.56303b37.svg"
+  }
+]);
